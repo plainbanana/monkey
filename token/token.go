0fb376275e9469fdbdadf65c251d37a8f11be8ef
plainbanana <kazukidegozaimasuruzo@gmail.com>
2019-01-21 22:57:36 +0900

    add simple lexer

---
+++ token/token.go
@@ -0,0 +1,49 @@
+package token
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// Tokentype :
+type TokenType string
+
+// Token :
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子＋リテラル
+	IDENT = "IDENT" // add, foobar ,x ,y, ...
+	INT   = "INT"   // 1234242
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// LookupIdent 識別子がIDENTか否かを判定する。IDENTでなければ正しいTokenTypeを返す
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
